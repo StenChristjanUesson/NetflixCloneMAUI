769eca7b568ae695aadeaf4e27e68c75066669fa
Opilane TTHK <34353363+StenChristjanUesson@users.noreply.github.com>
2025-05-26 16:39:55 +0300

    i got it up to date

--- NetflixCloneMAUI/App.xaml.cs
+++ NetflixCloneMAUI/App.xaml.cs
@@ -1,11 +1,13 @@
-﻿namespace NetflixCloneMAUI;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace NetflixCloneMAUI;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        MainPage = new AppShell();
+    }
 }
