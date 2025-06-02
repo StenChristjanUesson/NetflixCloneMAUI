5c7f6b2cbe297b5c5edc9f0877f57991eee15faf
Opilane TTHK <34353363+StenChristjanUesson@users.noreply.github.com>
2025-06-02 12:44:59 +0300

    part 5

--- NetflixCloneMaui/Controls/MovieRow.xaml.cs
+++ NetflixCloneMaui/Controls/MovieRow.xaml.cs
@@ -1,9 +1,39 @@
-namespace NetflixCloneMaui.Controls;
+using NetflixCloneMAUI.Models;
 
-public partial class MovieRow : ContentPage
+namespace NetflixCloneMAUI.Controls;
+
+public partial class MovieRow : ContentView
 {
-	public MovieRow()
-	{
-		InitializeComponent();
-	}
+    public static readonly BindableProperty HeadingProperty =
+            BindableProperty.Create(nameof(Heading), typeof(string), typeof(MovieRow), string.Empty);
+
+    public static readonly BindableProperty MoviesProperty =
+            BindableProperty.Create(nameof(Movies), typeof(IEnumerable<Media>), typeof(MovieRow), Enumerable.Empty<Media>());
+
+    public static readonly BindableProperty IsLargeProperty =
+            BindableProperty.Create(nameof(IsLarge), typeof(bool), typeof(MovieRow), false);
+
+    public MovieRow()
+    {
+        InitializeComponent();
+    }
+
+
+    public string Heading
+    {
+        get => (string)GetValue(MovieRow.HeadingProperty);
+        set => SetValue(MovieRow.HeadingProperty, value);
+    }
+    public IEnumerable<Media> Movies
+    {
+        get => (IEnumerable<Media>)GetValue(MovieRow.MoviesProperty);
+        set => SetValue(MovieRow.MoviesProperty, value);
+    }
+    public bool IsLarge
+    {
+        get => (bool)GetValue(MovieRow.IsLargeProperty);
+        set => SetValue(MovieRow.IsLargeProperty, value);
+    }
+
+    public bool IsNotLarge => !IsLarge;
 }
